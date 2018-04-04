842f9d876ac68d9ca934dfe2a06229402bdc8d3e
KowaiiNeko <nawhpongmaji1@gmail.com>
2018-04-04 00:35:09 -0500

    Update golden_boy.py

--- golden_boy.py
+++ golden_boy.py
@@ -16,7 +16,7 @@ import os
 
 
 
-api = os.environ['API_KEY']
+# api = os.environ['API_KEY']
 
 
 
@@ -99,78 +99,78 @@ async def kick(ctx, user: discord.Member):
 
 
 
-@bot.command(pass_context=True)
-async def summoner(ctx):
-    raw_name = ctx.message.content.split("~summoner ")
-    name = " ".join(raw_name[1:])
-    """GETS THE SUMMONER'S BASIC INFORMATION; NAME,LEVEL"""
-    link = rq.get("https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/{}?api_key={}".format(name, api)).text
-    rq_json = json.loads(link)
-    await bot.say("{}is level: {}\n{}'s profile icon is: {}".format(rq_json['name'],rq_json['summonerLevel'],rq_json['name'],rq_json['profileIconId']))
+# @bot.command(pass_context=True)
+# async def summoner(ctx):
+#     raw_name = ctx.message.content.split("~summoner ")
+#     name = " ".join(raw_name[1:])
+#     """GETS THE SUMMONER'S BASIC INFORMATION; NAME,LEVEL"""
+#     link = rq.get("https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/{}?api_key={}".format(name, api)).text
+#     rq_json = json.loads(link)
+#     await bot.say("{}is level: {}\n{}'s profile icon is: {}".format(rq_json['name'],rq_json['summonerLevel'],rq_json['name'],rq_json['profileIconId']))
 
 
-@bot.command(pass_context=True)
-async def lore(ctx):
-    """GETS THE LORE OF A CHAMPION GIVEN"""
-    champ_name = ctx.message.content.split("~lore ")
-    new_msg = " ".join(champ_name[1:]).lower()
-    champ = rq.get('https://na1.api.riotgames.com/lol/static-data/v3/champions/{}?locale=en_US&champData=lore&api_key={}'.format(champs['keys'][new_msg],api)).text
-    champ_json=json.loads(champ)
-    await bot.say("Champion Name: {}\nTitle: {}\nLore: {}".format(champ_json['name'],champ_json['title'],champ_json['lore']))
-
-
-
-@bot.command(pass_context=True)
-async def masterytotal(ctx):
-    """GETS THE SUMMONER'S TOTAL MASTERY POINTS"""
-    raw_name=ctx.message.content.split("~masterytotal ")
-    name=" ".join(raw_name[1:]).lower()
-    link = rq.get("https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/{}?api_key={}".format(name, api)).text
-    rq_json = json.loads(link)
-    ide = rq_json['id']
-    mast = rq.get("https://na1.api.riotgames.com/lol/champion-mastery/v3/scores/by-summoner/{}?api_key={}".format(ide, api)).text
-    await bot.say("{}'s total mastery points is: {}".format(name,mast))
+# @bot.command(pass_context=True)
+# async def lore(ctx):
+#     """GETS THE LORE OF A CHAMPION GIVEN"""
+#     champ_name = ctx.message.content.split("~lore ")
+#     new_msg = " ".join(champ_name[1:]).lower()
+#     champ = rq.get('https://na1.api.riotgames.com/lol/static-data/v3/champions/{}?locale=en_US&champData=lore&api_key={}'.format(champs['keys'][new_msg],api)).text
+#     champ_json=json.loads(champ)
+#     await bot.say("Champion Name: {}\nTitle: {}\nLore: {}".format(champ_json['name'],champ_json['title'],champ_json['lore']))
 
 
 
-
-
-@bot.command(pass_context=True)
-async def rank(ctx):
-    """GETS THE SUMMONER'S RANK INFO, ONLY SOLODUO"""
-    try:
-        raw_name=ctx.message.content.split("~rank ")
-        name=" ".join(raw_name[1:])
-        link = rq.get("https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/{}?api_key={}".format(name, api)).text
-        rq_json = json.loads(link)
-        ide = rq_json['id']
-        link2 = rq.get("https://na1.api.riotgames.com/lol/league/v3/positions/by-summoner/{}?api_key={}".format(ide,api)).text
-        rq_json1 = json.loads(link2)
-        #solo/duo rank info
-        solo_rank = rq_json1[1]['queueType']
-        league_name_solo = rq_json1[1]['leagueName']
-        tier_solo = rq_json1[1]['tier']
-        wins_solo = rq_json1[1]['wins']
-        losses_solo = rq_json1[1]['losses']
-        division_solo = rq_json1[1]['rank']
-        points_solo = rq_json1[1]['leaguePoints']
-        await bot.say("Rank Type: {}\nLeague Name: {}\nTier: {}\nWins: {}\nLosses: {}\nDivision: {}\nPoints: {}".format(solo_rank,league_name_solo,tier_solo,wins_solo,losses_solo,division_solo,points_solo))
-    except IndexError:
-        if link2 == "[]":
-            await bot.say("Summoner {} is not ranked".format(name))
-
-    except:
-        solo_rank = rq_json1[0]['queueType']
-        league_name_solo = rq_json1[0]['leagueName']
-        tier_solo = rq_json1[0]['tier']
-        wins_solo = rq_json1[0]['wins']
-        losses_solo = rq_json1[0]['losses']
-        division_solo = rq_json1[0]['rank']
-        points_solo = rq_json1[0]['leaguePoints']
-        await bot.say("Rank Type: {}\nLeague Name: {}\nTier: {}\nWins: {}\nLosses: {}\nDivision: {}\nPoints: {}".format(solo_rank, league_name_solo, tier_solo, wins_solo,losses_solo, division_solo, points_solo))
+# @bot.command(pass_context=True)
+# async def masterytotal(ctx):
+#     """GETS THE SUMMONER'S TOTAL MASTERY POINTS"""
+#     raw_name=ctx.message.content.split("~masterytotal ")
+#     name=" ".join(raw_name[1:]).lower()
+#     link = rq.get("https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/{}?api_key={}".format(name, api)).text
+#     rq_json = json.loads(link)
+#     ide = rq_json['id']
+#     mast = rq.get("https://na1.api.riotgames.com/lol/champion-mastery/v3/scores/by-summoner/{}?api_key={}".format(ide, api)).text
+#     await bot.say("{}'s total mastery points is: {}".format(name,mast))
 
 
 
 
 
-bot.run(os.environ['BOT_TOKEN'])
+# @bot.command(pass_context=True)
+# async def rank(ctx):
+#     """GETS THE SUMMONER'S RANK INFO, ONLY SOLODUO"""
+#     try:
+#         raw_name=ctx.message.content.split("~rank ")
+#         name=" ".join(raw_name[1:])
+#         link = rq.get("https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/{}?api_key={}".format(name, api)).text
+#         rq_json = json.loads(link)
+#         ide = rq_json['id']
+#         link2 = rq.get("https://na1.api.riotgames.com/lol/league/v3/positions/by-summoner/{}?api_key={}".format(ide,api)).text
+#         rq_json1 = json.loads(link2)
+#         #solo/duo rank info
+#         solo_rank = rq_json1[1]['queueType']
+#         league_name_solo = rq_json1[1]['leagueName']
+#         tier_solo = rq_json1[1]['tier']
+#         wins_solo = rq_json1[1]['wins']
+#         losses_solo = rq_json1[1]['losses']
+#         division_solo = rq_json1[1]['rank']
+#         points_solo = rq_json1[1]['leaguePoints']
+#         await bot.say("Rank Type: {}\nLeague Name: {}\nTier: {}\nWins: {}\nLosses: {}\nDivision: {}\nPoints: {}".format(solo_rank,league_name_solo,tier_solo,wins_solo,losses_solo,division_solo,points_solo))
+#     except IndexError:
+#         if link2 == "[]":
+#             await bot.say("Summoner {} is not ranked".format(name))
+
+#     except:
+#         solo_rank = rq_json1[0]['queueType']
+#         league_name_solo = rq_json1[0]['leagueName']
+#         tier_solo = rq_json1[0]['tier']
+#         wins_solo = rq_json1[0]['wins']
+#         losses_solo = rq_json1[0]['losses']
+#         division_solo = rq_json1[0]['rank']
+#         points_solo = rq_json1[0]['leaguePoints']
+#         await bot.say("Rank Type: {}\nLeague Name: {}\nTier: {}\nWins: {}\nLosses: {}\nDivision: {}\nPoints: {}".format(solo_rank, league_name_solo, tier_solo, wins_solo,losses_solo, division_solo, points_solo))
+
+
+
+
+
+bot.run('MzE3MDkyNzg4Mzc2NDM2NzM2.DaX1VA.ULX1Eda4dMNVkv44VX2506V1jco')
