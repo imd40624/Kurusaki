55f5c99d1e19884938acf5af8451c47c74f2e9e9
CharmingMother <nawhpongmaji1@gmail.com>
2018-04-16 14:37:21 -0500

    Update apiai.py

--- apiai.py
+++ apiai.py
@@ -9,28 +9,44 @@ bot=commands.Bot(command_prefix="$")
 
 @bot.event
 async def on_ready():
-  print(bot.user.name+mention=bot.user.mention)
+  print(bot.mention)
+  print(bot.user.name)
+  
+  
+  
   
   
 # @bot.event
 # async def on_message(message):
 #   mention=bot.user.mention
-#   raw_msg=message.content.split("")
+#   if message.content.startswith(mention):
+#     raw_msg=message.content.split(mention)
+#     msg="".join(raw_msg[1:])
+#     client_token='5df92cae29c5452aadd286f3001112c4'
+#     ai=apiai.ApiAI(client_token)
+#     request =ai.text_request()
+#     request.lang= 'en'
+#     request.session_id = "<SESSION ID, UNIQUE FOR EACH USER>"
+#     request.query = msg
+#     response = request.getresponse()
+#     rope = str(response.read())
+#     rope = rope[rope.index("speech")+10:]
+#     rope = rope[0:rope.index("\"")]
+#     await bot.say(rope)
 
-
-
-import apiai
-def chatbot(arg):
-  client_token='5df92cae29c5452aadd286f3001112c4'
-  ai=apiai.ApiAI(client_token)
-  request =ai.text_request()
-  request.lang= 'en'
-  request.session_id = "<SESSION ID, UNIQUE FOR EACH USER>"
-  request.query = arg
-  response = request.getresponse()
-  rope = str(response.read())
-  rope = rope[rope.index("speech")+10:]
-  rope = rope[0:rope.index("\"")]
-  print(rope)
+# def chatbot(arg):
+#   client_token='5df92cae29c5452aadd286f3001112c4'
+#   ai=apiai.ApiAI(client_token)
+#   request =ai.text_request()
+#   request.lang= 'en'
+#   request.session_id = "<SESSION ID, UNIQUE FOR EACH USER>"
+#   request.query = arg
+#   response = request.getresponse()
+#   rope = str(response.read())
+#   rope = rope[rope.index("speech")+10:]
+#   rope = rope[0:rope.index("\"")]
+#   print(rope)
   
-chatbot("hello")
+# chatbot("hello")
+
+bot.run('NDA1OTA3MzI2MDUxMDI0OTA2.DbaJsg.y5RgLat4y2295thK2ftjkd6EJkQ')
