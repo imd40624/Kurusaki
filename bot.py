0eeddf28e1a34e4c84e4fb65409e3874c2e6bd72
CharmingMother <nawhpongmaji1@gmail.com>
2018-04-16 19:59:26 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -34,29 +34,6 @@ async def on_ready():
 
 
 
-@bot.event
-async def on_message(message):
-    mention = bot.user.mention
-    if message.content.startswith(mention):
-        raw_msg = message.content.split("{}".format(mention))
-        msg = "".join(raw_msg[1:])
-        #         print(msg)
-        client_token = os.environ['api_ai']
-        ai = apiai.ApiAI(client_token)
-        request = ai.text_request()
-        request.lang = 'en'
-        request.session_id = "<SESSION ID, UNIQUE FOR EACH USER>"
-        request.query = msg
-        response = request.getresponse()
-        rope = str(response.read())
-        rope = rope[rope.index("speech") + 10:]
-        rope = rope[0:rope.index("\"")]
-        await bot.send_message(message.channel, rope)
-
-
-
-
-
 
 
 
@@ -422,6 +399,28 @@ async def urban(ctx):
     rq_json=json.loads(rq_link)
     await bot.say("Word: {}\nVotes: {}\nDefinitioin: {}\nExample: {}".format(rq_json['list'][0]['word'],rq_json['list'][0]['thumbs_up'],rq_json['list'][0]['definition'],rq_json['list'][0]['example']))
 
+    
+    
+@bot.event
+async def on_message(message):
+    mention = bot.user.mention
+    if message.content.startswith(mention):
+        raw_msg = message.content.split("{}".format(mention))
+        msg = "".join(raw_msg[1:])
+        #         print(msg)
+        client_token = os.environ['api_ai']
+        ai = apiai.ApiAI(client_token)
+        request = ai.text_request()
+        request.lang = 'en'
+        request.session_id = "<SESSION ID, UNIQUE FOR EACH USER>"
+        request.query = msg
+        response = request.getresponse()
+        rope = str(response.read())
+        rope = rope[rope.index("speech") + 10:]
+        rope = rope[0:rope.index("\"")]
+        await bot.send_message(message.channel, rope)
+
+    
 
 
 bot.run(os.environ['BOT_TOKEN'])
