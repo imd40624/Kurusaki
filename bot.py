327355022727dd67f99e8d5fd3c194f12be186c7
CharmingMother <nawhpongmaji1@gmail.com>
2018-04-16 20:32:43 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -400,26 +400,7 @@ async def urban(ctx):
     await bot.say("Word: {}\nVotes: {}\nDefinitioin: {}\nExample: {}".format(rq_json['list'][0]['word'],rq_json['list'][0]['thumbs_up'],rq_json['list'][0]['definition'],rq_json['list'][0]['example']))
 
     
-    
-@bot.event
-async def on_message(message):
-    mention = bot.user.mention
-    if message.content.startswith(mention):
-        raw_msg = message.content.split("{}".format(mention))
-        msg = "".join(raw_msg[1:])
-        #         print(msg)
-        client_token = os.environ['api_ai']
-        ai = apiai.ApiAI(client_token)
-        request = ai.text_request()
-        request.lang = 'en'
-        request.session_id = "<SESSION ID, UNIQUE FOR EACH USER>"
-        request.query = msg
-        response = request.getresponse()
-        rope = str(response.read())
-        rope = rope[rope.index("speech") + 10:]
-        rope = rope[0:rope.index("\"")]
-        await bot.send_message(message.channel, rope)
-
+  
     
 
 
