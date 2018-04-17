b78fb576919b31f6c7fc1b3d175343bc48f456e3
CharmingMother <nawhpongmaji1@gmail.com>
2018-04-16 23:21:25 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -32,24 +32,24 @@ async def on_ready():
     await bot.change_presence(game=discord.Game(name=mesg))
 
 
-@bot.event
-async def on_message(message):
-    mention = bot.user.mention
-    if message.content.startswith(mention):
-        raw_msg = message.content.split("{}".format(mention))
-        msg = "".join(raw_msg[1:])
-        #         print(msg)
-        client_token = os.environ['api_ai']
-        ai = apiai.ApiAI(client_token)
-        request = ai.text_request()
-        request.lang = 'en'
-        request.session_id = "<SESSION ID, UNIQUE FOR EACH USER>"
-        request.query = msg
-        response = request.getresponse()
-        rope = str(response.read())
-        rope = rope[rope.index("speech") + 10:]
-        rope = rope[0:rope.index("\"")]
-        await bot.send_message(message.channel, rope)
+# @bot.event
+# async def on_message(message):
+#     mention = bot.user.mention
+#     if message.content.startswith(mention):
+#         raw_msg = message.content.split("{}".format(mention))
+#         msg = "".join(raw_msg[1:])
+#         #         print(msg)
+#         client_token = os.environ['api_ai']
+#         ai = apiai.ApiAI(client_token)
+#         request = ai.text_request()
+#         request.lang = 'en'
+#         request.session_id = "<SESSION ID, UNIQUE FOR EACH USER>"
+#         request.query = msg
+#         response = request.getresponse()
+#         rope = str(response.read())
+#         rope = rope[rope.index("speech") + 10:]
+#         rope = rope[0:rope.index("\"")]
+#         await bot.send_message(message.channel, rope)
 
 
 
