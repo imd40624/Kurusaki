2415afc7f8676df2fb6667a61a592fc7445e060f
CharmingMother <nawhpongmaji1@gmail.com>
2018-04-27 15:15:39 -0500

    Update tools.py

--- tools.py
+++ tools.py
@@ -2,6 +2,11 @@ import requests as rq
 import json
 
 
+
+wu_key=os.environ['WU_API']
+owm=os.environ['open_weather']
+
+
 class rq_ra():
   ra=rq.get('https://private-anon-589c768a77-popcornofficial.apiary-proxy.com/random/anime')
   ra1=rq.get('https://tv-v2.api-fetch.website/random/anime')
@@ -10,8 +15,14 @@ class rq_ra():
   title=rq_json['title']
   
 class Weather():
-  opwm=''
-  url=''
-  r=rq.get(url).text
-  r_json=json.loads(r)
-  
+  t = u"\u00b0"
+  remove_command = ctx.message.content.split("a.weather ")
+  city_state = " ".join(remove_command[1:])
+  url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid={}'.format(city_state,owm)
+  ser = rq.get(url).text
+  rq_json = json.loads(ser)   
+  temp = rq_json['main']['temp']
+  max_temp = rq_json['main']['temp_max']
+  min_temp = rq_json['main']['temp_min']
+  hum = rq_json['main']['humidity']
+  wind = rq_json['wind']['speed']
