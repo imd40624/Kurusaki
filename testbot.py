0a0232eded9f401bdf33d7a88b5aa0244fd89cd6
CharmingMother <nawhpongmaji1@gmail.com>
2018-05-14 09:27:40 -0500

    Update testbot.py

--- testbot.py
+++ testbot.py
@@ -11,7 +11,7 @@ from oauth2client.service_account import ServiceAccountCredentials
 scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
 
 
-credentials=ServiceAccountCredentials.from_json_keyfile_name("Annie-e432eb58860b.json".scope)
+credentials=ServiceAccountCredentials.from_json_keyfile_name("Annie-e432eb58860b.json",scope)
 gc= gspread.authorize(credentials)
 wks=gc.open("Kurusaki_database_discord".sheet1)
 
