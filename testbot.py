ef62f55ecc0b95e43f66c19e68aef45dffeb82ec
CharmingMother <nawhpongmaji1@gmail.com>
2018-05-14 09:31:36 -0500

    Update testbot.py

--- testbot.py
+++ testbot.py
@@ -1,9 +1,9 @@
-# import discord
-# import discord.ext
-# from discord.ext import commands
-# from discord.ext.commands import Bot
-# import asyncio
-# import json
+import discord
+import discord.ext
+from discord.ext import commands
+from discord.ext.commands import Bot
+import asyncio
+import json
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
@@ -17,33 +17,32 @@ wks=gc.open("Kurusaki_database_discord").sheet1
 
 
 print(wks.get_all_records())
+ex_result='[{'Discord Name': 'Dong Cheng', 'ID': 185181025104560128, 'Points': 1}]'
 
-# bot = commands.Bot(command_prefix='a.')
+bot = commands.Bot(command_prefix='a.')
 
-# @bot.event
-# async def on_ready():
-#     """WHEN BOT IS READY, PRINT MESSAGE IN TERMINAL"""
-#     print ("I am running on " + bot.user.name)
-#     mesg="Hello Kitty"
-#     await bot.change_presence(game=discord.Game(name=mesg))
-
+@bot.event
+async def on_ready():
+    """WHEN BOT IS READY, PRINT MESSAGE IN TERMINAL"""
+    print ("I am running on " + bot.user.name)
 
 
 
 
 
-# @bot.event
-# async def on_message(message):
-#   try:
-#     msg=message.content
-#     if message.author.id in wks:
-#       await bot.say("Someone has messaged!")
-#     elif message.author.id not in wks:
-#       add_user=wks.append(message.author.name,message.author.id,".1")
-#       await bot.say("Author id is not in database yet, let's add it")
-#   except:
-#     await bot.say("looks like it didn't work")
+
+@bot.event
+async def on_message(message):
+  try:
+    msg=message.content
+    if message.author.id in wks:
+      await bot.say("Someone has messaged!")
+    elif message.author.id not in wks:
+      add_user=wks.append(message.author.name,message.author.id,".1")
+      await bot.say("Author id is not in database yet, let's add it")
+  except:
+    await bot.say("looks like it didn't work")
     
 
 
-# bot.run('MzE3MDkyNzg4Mzc2NDM2NzM2.DddL0Q.hF6RoIJ2bzXYHq_4H9ndlZAgfis')
+bot.run('MzE3MDkyNzg4Mzc2NDM2NzM2.DddL0Q.hF6RoIJ2bzXYHq_4H9ndlZAgfis')
