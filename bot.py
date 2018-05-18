90fa201b703664b3c0511878a7fd2bc3fcfb57b7
CharmingMother <nawhpongmaji1@gmail.com>
2018-05-18 10:24:44 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -36,7 +36,7 @@ bot = commands.Bot(command_prefix='s.')
 async def on_ready():
     """WHEN BOT IS READY, PRINT MESSAGE IN TERMINAL"""
     print("I am running on " + bot.user.name)
-    mesg =tools.Games.random_game
+    mesg =tools.Games.game
     await bot.change_presence(game=discord.Game(name=mesg))
 
 
