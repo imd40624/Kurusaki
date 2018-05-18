129260de7654ddb5ae848d187d2a5d188569a402
CharmingMother <nawhpongmaji1@gmail.com>
2018-05-18 14:24:51 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -35,12 +35,12 @@ bot = commands.Bot(command_prefix='s.')
 async def on_ready():
     """WHEN BOT IS READY, PRINT MESSAGE IN TERMINAL"""
     print("I am running on " + bot.user.name)
-    while True:
-        games=['Bread Puppies','Jump Rope Kitten: Nyawatobi','TripTrap','Potion Maker','Crusaders Quest','My Waffle Maker','AfroCat','Hello Kitty','Halo 4','My Cat Album','LINE: Disney Tsum Tsum','Cat Room','Alphabear','Play With Cats','My Dog Album','Giant Turnip Game','MEOW MEOW STAR ACRES','Patchmania','Tiny Sheep','Hello Kitty World – Fun Park Game']
-        game=random.choice(games)
-        global random_game_change
-        random_game_change=str(game)
-    await bot.change_presence(game=discord.Game(name=random_game_change))
+#     while True:
+#         games=['Bread Puppies','Jump Rope Kitten: Nyawatobi','TripTrap','Potion Maker','Crusaders Quest','My Waffle Maker','AfroCat','Hello Kitty','Halo 4','My Cat Album','LINE: Disney Tsum Tsum','Cat Room','Alphabear','Play With Cats','My Dog Album','Giant Turnip Game','MEOW MEOW STAR ACRES','Patchmania','Tiny Sheep','Hello Kitty World – Fun Park Game']
+#         game=random.choice(games)
+#         global random_game_change
+#         random_game_change=str(game)
+    await bot.change_presence(game=discord.Game(name="My Cat Album"))
 
 
 @bot.event
