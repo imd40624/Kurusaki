e1b78f82baf5ab2192b3a4df54b6b01f009d73f9
CharmingMother <nawhpongmaji1@gmail.com>
2018-05-18 09:32:13 -0500

    Update status.py

--- status.py
+++ status.py
@@ -3,11 +3,10 @@ from oauth2client.service_account import ServiceAccountCredentials
 
 
 
-
-scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
-credentials = ServiceAccountCredentials.from_json_keyfile_name('Annie-e432eb58860b.json', scope)
-gc = gspread.authorize(credentials)
-wks = gc.open('Kurusaki_database_discord').sheet2
-
-
-
+def checking():
+  scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+  credentials = ServiceAccountCredentials.from_json_keyfile_name('Annie-e432eb58860b.json', scope)
+  gc = gspread.authorize(credentials)
+  wks = gc.open('Kurusaki_database_discord').time
+  ald=wks.get_all_records()
+  print(ald)
