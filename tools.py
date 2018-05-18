5041614b0f99927c354a12f5912fcbb075707039
CharmingMother <nawhpongmaji1@gmail.com>
2018-05-18 10:30:11 -0500

    Update tools.py

--- tools.py
+++ tools.py
@@ -31,6 +31,7 @@ class Games():
   while True:
     games=['Bread Puppies','Jump Rope Kitten: Nyawatobi','TripTrap','Potion Maker','Crusaders Quest','My Waffle Maker','AfroCat','Hello Kitty','Halo 4','My Cat Album','LINE: Disney Tsum Tsum','Cat Room','Alphabear','Play With Cats','My Dog Album','Giant Turnip Game','MEOW MEOW STAR ACRES','Patchmania','Tiny Sheep','Hello Kitty World – Fun Park Game']
     random_game=random.choice(games)
+    global game
     game=str(random_game)
     time.sleep(2700)
   
