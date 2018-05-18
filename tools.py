3e8127707b6e60d1d351c7f193421bc04b7c2d4a
CharmingMother <nawhpongmaji1@gmail.com>
2018-05-18 10:24:26 -0500

    Update tools.py

--- tools.py
+++ tools.py
@@ -32,4 +32,5 @@ class Games():
     games=['Bread Puppies','Jump Rope Kitten: Nyawatobi','TripTrap','Potion Maker','Crusaders Quest','My Waffle Maker','AfroCat','Hello Kitty','Halo 4','My Cat Album','LINE: Disney Tsum Tsum','Cat Room','Alphabear','Play With Cats','My Dog Album','Giant Turnip Game','MEOW MEOW STAR ACRES','Patchmania','Tiny Sheep','Hello Kitty World – Fun Park Game']
     random_game=random.choice(games)
     time.sleep(2700)
+    game=random_game
   
