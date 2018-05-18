32b26ff07883c6f91bad3741af9cc2e21c52f13c
CharmingMother <nawhpongmaji1@gmail.com>
2018-05-18 13:33:38 -0500

    Create new.py

---
+++ new.py
@@ -0,0 +1,18 @@
+import discord
+from discord.ext import commands
+from discord.ext.commands import Bot
+import asyncio
+imoirt time
+
+
+
+
+bot =commands.Bot(command_prefix='v')
+
+@bot.event
+async def on_ready():
+  print("Bot {} is ready to run".format(bot.user.name))
+
+
+
+bot.run('NDAzNDAyNjE0NDU0MzUzOTQx.DeCrDw.zyBWdRkVggjETQraJhGTgTHRWuo')
