06792742bb8544e25bd236c5380e1d1386f05fe1
CharmingMother <nawhpongmaji1@gmail.com>
2018-05-20 16:56:12 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -36,12 +36,12 @@ async def on_ready():
     
     """WHEN BOT IS READY, PRINT MESSAGE IN TERMINAL"""
     print("I am running on " + bot.user.name)
-    games=['Bread Puppies','Jump Rope Kitten: Nyawatobi','TripTrap','Potion Maker','Crusaders Quest','My Waffle Maker','AfroCat','Hello Kitty','Halo 4','My Cat Album','LINE: Disney Tsum Tsum','Cat Room','Alphabear','Play With Cats','My Dog Album','Giant Turnip Game','MEOW MEOW STAR ACRES','Patchmania','Tiny Sheep','Hello Kitty World – Fun Park Game']
-    while True:
-        global random_status_game
-        random_status_game=random.choice(games)
-        asyncio.sleep(2000)
-    await bot.change_presence(game=discord.Game(name=random_status_game))
+#     games=['Bread Puppies','Jump Rope Kitten: Nyawatobi','TripTrap','Potion Maker','Crusaders Quest','My Waffle Maker','AfroCat','Hello Kitty','Halo 4','My Cat Album','LINE: Disney Tsum Tsum','Cat Room','Alphabear','Play With Cats','My Dog Album','Giant Turnip Game','MEOW MEOW STAR ACRES','Patchmania','Tiny Sheep','Hello Kitty World – Fun Park Game']
+#     while True:
+#         global random_status_game
+#         random_status_game=random.choice(games)
+#         asyncio.sleep(2000)
+    await bot.change_presence(game=discord.Game(name="MEOW MEOW STAR ACRES"))
 
 
 @bot.event
