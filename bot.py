89f59e315f06327990e7382b937af28075ae6c07
CharmingMother <nawhpongmaji1@gmail.com>
2018-05-22 14:33:32 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -47,37 +47,37 @@ async def on_ready():
 @bot.event
 async def on_message(message):
     import apiai_set
-#     mention = bot.user.mention
-#     if message.content.startswith(mention):
-#         anime = rq.get('https://kurusaki-webhook.herokuapp.com/').text
-#         raw_msg = message.content.split("{}".format(mention))
-#         msg = "".join(raw_msg[1:])
-#         ai = apiai.ApiAI(apiai_token)
-#         request = ai.text_request()
-#         request.lang = 'en'
-#         request.session_id = "<SESSION ID, UNIQUE FOR EACH USER>"
-#         request.query = msg
-#         response = request.getresponse()
-#         rope = str(response.read())
-#         rope = rope[rope.index("speech") + 10:]
-#         rope = rope[0:rope.index("\"")]
-#         if '$anime' in rope:
-#             anime = rope.replace('$anime', anime)
-#             await bot.send_message(message.channel, anime)
-#         elif 'anime' not in rope:
-#             await bot.send_message(message.channel, rope)
-#         if "$time" in rope:
-#             await bot.say(datetime.datetime.now())
-#         if '$yukinno' in rope:
-#             author_id = message.author.id
-#             if author_id == 287369884940238849:
-#                 love = ['I love you Yukinno',
-#                         'Yukkino, I love you!', 'I love you!', '<3']
-#                 ran_love = random.choice(love)
-#                 yukinno_love = rope.replace(rope, ran_love)
-#                 await bot.send_message(message.channel, yukinno_love)
-#             elif message.author.id != 287369884940238849:
-#                 await bot.send_message(message.channel, rope)
+    mention = bot.user.mention
+    if message.content.startswith(mention):
+        anime = rq.get('https://kurusaki-webhook.herokuapp.com/').text
+        raw_msg = message.content.split("{}".format(mention))
+        msg = "".join(raw_msg[1:])
+        ai = apiai.ApiAI(apiai_token)
+        request = ai.text_request()
+        request.lang = 'en'
+        request.session_id = "<SESSION ID, UNIQUE FOR EACH USER>"
+        request.query = msg
+        response = request.getresponse()
+        rope = str(response.read())
+        rope = rope[rope.index("speech") + 10:]
+        rope = rope[0:rope.index("\"")]
+        if '$anime' in rope:
+            anime = rope.replace('$anime', anime)
+            await bot.send_message(message.channel, anime)
+        elif 'anime' not in rope:
+            await bot.send_message(message.channel, rope)
+        if "$time" in rope:
+            await bot.say(datetime.datetime.now())
+        if '$yukinno' in rope:
+            author_id = message.author.id
+            if author_id == 287369884940238849:
+                love = ['I love you Yukinno',
+                        'Yukkino, I love you!', 'I love you!', '<3']
+                ran_love = random.choice(love)
+                yukinno_love = rope.replace(rope, ran_love)
+                await bot.send_message(message.channel, yukinno_love)
+            elif message.author.id != 287369884940238849:
+                await bot.send_message(message.channel, rope)
     scope = ['https://spreadsheets.google.com/feeds',
              'https://www.googleapis.com/auth/drive']
     credentials = ServiceAccountCredentials.from_json_keyfile_name(
