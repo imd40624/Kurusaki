b27d1caf632586550e8dd44ba9d338b707575479
imd40624 <46713870+imd40624@users.noreply.github.com>
2019-01-19 11:00:06 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,7 +14,6 @@ import random
 import time
 import urllib
 import urllib.request
-import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 from discord.utils import get
 
